d2fd9fefa881ef60f5b11986e70887f7af4bac78
Isa <isabelgp.ins@gmail.com>
2026-05-01 23:18:10 +0200

    Update TestGrafoConocimiento.java

--- src/Seleccionadas/grafos/TestGrafoConocimiento.java
+++ src/Seleccionadas/grafos/TestGrafoConocimiento.java
@@ -5,7 +5,7 @@ import Descartadas.Isabel.TADs.Lista;
 public class TestGrafoConocimiento {
     public static void main(String[] args) {
 
-        String ruta = "src/EL2b/DatosConsultaGrafo.json";
+        String ruta = "src/Seleccionadas/grafos/DatosGrafoConocimiento.json";
 
         GrafoDeConocimiento grafo = new GrafoDeConocimiento();
         ParserJSON.cargarGrafo(ruta, grafo);
